9b71f4cc5fe05bb22dd6b2311997ecaa6f9e162f
avalontm <avalontm21@gmail.com>
2021-05-06 16:54:13 -0700

    updated

---
+++ TMCreatureEditor/Helpers/Extentions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace TMCreatureEditor.Helpers
+{
+    public static class Extentions
+    {
+        public static Stream ToStream(this ImageSource source)
+        {
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create((BitmapSource)source));
+            MemoryStream stream = new MemoryStream();
+            encoder.Save(stream);
+
+            return stream;
+        }
+
+        public static ImageSource ToImage(this byte[] byteArray)
+        {
+            try
+            {
+                Image image = new Image();
+                using (MemoryStream stream = new MemoryStream(byteArray))
+                {
+                    image.Source = BitmapFrame.Create(stream, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+                    return image.Source;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+       
+    }
+}
---
+++ TMCreatureEditor/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TMCreatureEditor.Helpers;
+using TMFormat.Formats;
+
+namespace TMCreatureEditor
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        #region Propiedades
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        TMCreature _creature;
+
+        public TMCreature creature
+        {
+            get { return _creature; }
+            set { _creature = value;
+                OnPropertyChanged("creature");
+            }
+        }
+
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        void onLoaded(object sender, RoutedEventArgs e)
+        {
+            creature = new TMCreature();
+
+        }
+
+        void onUnloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void onNew(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void onOpen(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "TMF files (*.tmc)|*.tmc|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string _file = openFileDialog.FileName;
+
+                if (!File.Exists(_file))
+                {
+                    return;
+                }
+
+                creature = TMCreature.Load(_file);
+
+                if (creature == null)
+                {
+                    return;
+                }
+
+                Title = $"{creature.name} - {_file}";
+
+                // Texturas //
+                texture1.Source = creature.dirs[0].textures[0].ToImage();
+                texture2.Source = creature.dirs[0].textures[1].ToImage();
+                texture3.Source = creature.dirs[0].textures[2].ToImage();
+                texture4.Source = creature.dirs[0].textures[3].ToImage();
+
+                // Mascaras //
+                mask1.Source = creature.dirs[0].masks[0].ToImage();
+                mask2.Source = creature.dirs[0].masks[1].ToImage();
+                mask3.Source = creature.dirs[0].masks[2].ToImage();
+                mask4.Source = creature.dirs[0].masks[3].ToImage();
+            }
+        }
+
+        void onSave(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void onExit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
